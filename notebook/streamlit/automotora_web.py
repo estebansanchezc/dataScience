f7df1b7d4439adf9a68bd89f624f010dd3ad0b8a
estebansanchezc <estebansanchezl@gmail.com>
2023-03-08 23:41:04 -0300

    Update automotora_web.py
    porcentaje de ganancia

--- notebook/streamlit/automotora_web.py
+++ notebook/streamlit/automotora_web.py
@@ -136,30 +136,34 @@ def sesiones():
         st.session_state['mdl_premium7'] = None
     if 'mdl_premium8' not in st.session_state:
         st.session_state['mdl_premium8'] = None
+    if 'ganancia' not in st.session_state:
+        st.session_state['ganancia'] = None
+
 
 def porcentajeError(model_selected):
     riesgo = ''
-        
+
     if model_selected == 'rango desde 0 a 25.000':
-       riesgo = '7%'
+        riesgo = '7%'
     if model_selected == 'rango desde 25.001 a 35.000':
-       riesgo = '5%'
-    if model_selected == 'rango desde 35.0001 a 45.000':            
-       riesgo = '4%'
-    if model_selected == 'rango desde 45.001 a 55.000':             
-       riesgo = '3%'
-    if model_selected == 'rango desde 55.001 a 65.000':            
-       riesgo = '3%'
-    if model_selected == 'rango desde 65.001 a 75.000':            
-       riesgo = '3%'
-    if model_selected == 'rango desde 75.001 a 85.000':        
-       riesgo = '2%'
-    if model_selected == 'rango desde 85.001 a 95.000':    
-       riesgo = '2%'
-    if model_selected == 'mayores a 95.001':        
-       riesgo = '7%'       
-         
-    return riesgo   
+        riesgo = '5%'
+    if model_selected == 'rango desde 35.0001 a 45.000':
+        riesgo = '4%'
+    if model_selected == 'rango desde 45.001 a 55.000':
+        riesgo = '3%'
+    if model_selected == 'rango desde 55.001 a 65.000':
+        riesgo = '3%'
+    if model_selected == 'rango desde 65.001 a 75.000':
+        riesgo = '3%'
+    if model_selected == 'rango desde 75.001 a 85.000':
+        riesgo = '2%'
+    if model_selected == 'rango desde 85.001 a 95.000':
+        riesgo = '2%'
+    if model_selected == 'mayores a 95.001':
+        riesgo = '7%'
+
+    return riesgo
+
 
 def main():
     sesiones()
@@ -167,10 +171,10 @@ def main():
     #     mdlgeneralista = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_generalista.sav",'rb')
     #     mdl_generalista = pickle.load(mdlgeneralista)
     #     mdlgeneralista.close()
-    #     st.session_state.mdl_generalista = mdl_generalista
-    #     # mdl_generalista = pickle.load(
-    #     #   open('notebook/streamlit/modelos_serializados/RandomForestRegressor_generalista.sav', 'rb+'))
-    #     # st.session_state.mdl_generalista = mdl_generalista
+    # #     st.session_state.mdl_generalista = mdl_generalista
+    # #     # mdl_generalista = pickle.load(
+    # #     #   open('notebook/streamlit/modelos_serializados/RandomForestRegressor_generalista.sav', 'rb+'))
+    # #     # st.session_state.mdl_generalista = mdl_generalista
     # else:
     #     mdl_generalista = st.session_state.mdl_generalista
 
@@ -179,37 +183,40 @@ def main():
     #     mdl_premium1 = pickle.load(mdlpremium1)
     #     mdlpremium1.close()
     #     st.session_state.mdl_premium1 = mdl_premium1
-        
-    #     # mdl_premium1 = pickle.load(
-    #     # open('notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_1.sav', 'rb+'))
-    #     # st.session_state.mdl_premium1 = mdl_premium1
+
+    # #     # mdl_premium1 = pickle.load(
+    # #     # open('notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_1.sav', 'rb+'))
+    # #     # st.session_state.mdl_premium1 = mdl_premium1
     # else:
     #     mdl_premium1 = st.session_state.mdl_premium1
-    
-    if st.session_state.mdl_premium2 == None:            
+
+    if st.session_state.mdl_premium2 == None:
         #  mdl_premium2 = pickle.load(
         #  open('notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_2.sav', 'rb+'))
         #  st.session_state.mdl_premium2 = mdl_premium2
-        mdlpremium2 = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_2.sav",'rb')
+        mdlpremium2 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_2.sav", 'rb')
         mdl_premium2 = pickle.load(mdlpremium2)
         mdlpremium2.close()
         st.session_state.mdl_premium2 = mdl_premium2
     else:
         mdl_premium2 = st.session_state.mdl_premium2
-                
-    if st.session_state.mdl_premium3 is None:  
+
+    if st.session_state.mdl_premium3 is None:
         # mdl_premium3 = pickle.load(
         # open('notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_3.sav', 'rb+'))
         # st.session_state.mdl_premium3 = mdl_premium3
-        mdlpremium3 = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_3.sav",'rb')
+        mdlpremium3 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_3.sav", 'rb')
         mdl_premium3 = pickle.load(mdlpremium3)
         mdlpremium3.close()
         st.session_state.mdl_premium3 = mdl_premium3
     else:
         mdl_premium3 = st.session_state.mdl_premium3
-        
+
     if st.session_state.mdl_premium4 == None:
-        mdlpremium4 = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_4.sav",'rb')
+        mdlpremium4 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_4.sav", 'rb')
         mdl_premium4 = pickle.load(mdlpremium4)
         mdlpremium4.close()
         st.session_state.mdl_premium4 = mdl_premium4
@@ -218,9 +225,10 @@ def main():
         # st.session_state.mdl_premium3 = mdl_premium4
     else:
         mdl_premium4 = st.session_state.mdl_premium4
-    
-    if st.session_state.mdl_premium5 == None:       
-        mdlpremium5 = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_5.sav",'rb')
+
+    if st.session_state.mdl_premium5 == None:
+        mdlpremium5 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_5.sav", 'rb')
         mdl_premium5 = pickle.load(mdlpremium5)
         mdlpremium5.close()
         st.session_state.mdl_premium5 = mdl_premium5
@@ -229,48 +237,48 @@ def main():
         # st.session_state.mdl_premium5 = mdl_premium5
     else:
         mdl_premium5 = st.session_state.mdl_premium5
-        
-        
+
     if st.session_state.mdl_premium6 == None:
-        mdlpremium6 = bz2.BZ2File("notebook/streamlit/modelos_serializados/BaggingRegressor_premium_6.sav",'rb')
+        mdlpremium6 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/BaggingRegressor_premium_6.sav", 'rb')
         mdl_premium6 = pickle.load(mdlpremium6)
         mdlpremium6.close()
-        st.session_state.mdl_premium6 = mdl_premium6  
+        st.session_state.mdl_premium6 = mdl_premium6
         # mdl_premium6 = pickle.load(
         # open('notebook/streamlit/modelos_serializados/BaggingRegressor_premium_6.sav', 'rb+'))
         # st.session_state.mdl_premium6 = mdl_premium6
     else:
         mdl_premium6 = st.session_state.mdl_premium6
-    
+
     if st.session_state.mdl_premium7 == None:
-        mdlpremium7 = bz2.BZ2File("notebook/streamlit/modelos_serializados/GradientBoostingRegressor_premium_7.sav",'rb')
+        mdlpremium7 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/GradientBoostingRegressor_premium_7.sav", 'rb')
         mdl_premium7 = pickle.load(mdlpremium7)
         mdlpremium7.close()
         st.session_state.mdl_premium7 = mdl_premium7
-        
+
         # mdl_premium7 = pickle.load(open(
         # 'notebook/streamlit/modelos_serializados/GradientBoostingRegressor_premium_7.sav', 'rb+'))
         # st.session_state.mdl_premium7 = mdl_premium7
     else:
         mdl_premium7 = st.session_state.mdl_premium7
-    
+
     if st.session_state.mdl_premium8 == None:
-        mdlpremium8 = bz2.BZ2File("notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_8.sav",'rb')
+        mdlpremium8 = bz2.BZ2File(
+            "notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_8.sav", 'rb')
         mdl_premium8 = pickle.load(mdlpremium8)
         mdlpremium8.close()
         st.session_state.mdl_premium8 = mdl_premium8
-        
+
         # mdl_premium8 = pickle.load(
         # open('notebook/streamlit/modelos_serializados/RandomForestRegressor_premium_8.sav', 'rb+'))
         # st.session_state.mdl_premium8 = mdl_premium8
     else:
         mdl_premium8 = st.session_state.mdl_premium8
-        
-        
+
     # titulo
     st.title('Anaconda Predict')
     st.subheader('Ingreso de Datos')
-
 
     # titulo de sidebar
     st.sidebar.header('Seleccione')
@@ -283,11 +291,10 @@ def main():
             st.session_state.dataMarca = dataMarca
         else:
             dataMarca = st.session_state.dataMarca
-            
+
         select_make = st.selectbox(
             'Selecciona marca vehiculo 👇', options=(l.Make_Car for l in dataMarca))
-              
-                
+
         if st.session_state.dataModelos == None:
             dataModelos = cargarDataModelos(select_make)
             st.session_state.dataModelos = dataModelos
@@ -295,19 +302,18 @@ def main():
             dataModelos = st.session_state.dataModelos
             list_modelo = filter(lambda x: x.Make == select_make, dataModelos)
             dataModelos = list(list_modelo)
-        
-        time.sleep(2)    
-                            
+
+        time.sleep(2)
+
         select_model = st.selectbox(
             'Selecciona modelo vehiculo 👇', (l.Model_Car for l in dataModelos))
-        
+
         if st.session_state.dataEstados == None:
             dataEstados = cargarDataEstados()
             st.session_state.dataEstados = dataEstados
         else:
             dataEstados = st.session_state.dataEstados
-        
-        
+
         select_state = st.selectbox(
             'Selecciona estado 👇', (l.Name for l in dataEstados))
 
@@ -342,6 +348,9 @@ def main():
             min_value=1,
             max_value=10
         )
+
+        st.session_state.ganancia = st.number_input(
+            label="Porcentaje de ganancia", step=1., format="%.2f")
 
         # text_input_doors = st.number_input(
         #     label="ingrese cantidad de puertas 👇",
@@ -380,53 +389,54 @@ def main():
     df = user_input_parameters()
 
     # escoger el modelo preferido
-    # option = ['rango desde 0 a 25.000', 'rango desde 25.001 a 35.000', 
+    # option = ['rango desde 0 a 25.000', 'rango desde 25.001 a 35.000',
     #           'rango desde 35.0001 a 45.000', 'rango desde 45.001 a 55.000',
-    #           'rango desde 55.001 a 65.000', 'rango desde 65.001 a 75.000', 
-    #           'rango desde 75.001 a 85.000', 'rango desde 85.001 a 95.000', 
+    #           'rango desde 55.001 a 65.000', 'rango desde 65.001 a 75.000',
+    #           'rango desde 75.001 a 85.000', 'rango desde 85.001 a 95.000',
     #           'mayores a 95.001']
-    
-    option = ['rango desde 35.0001 a 45.000','rango desde 45.001 a 55.000',
-              'rango desde 55.001 a 65.000', 'rango desde 65.001 a 75.000', 
-              'rango desde 75.001 a 85.000', 'rango desde 85.001 a 95.000', 
+
+    option = ['rango desde 35.0001 a 45.000', 'rango desde 45.001 a 55.000',
+              'rango desde 55.001 a 65.000', 'rango desde 65.001 a 75.000',
+              'rango desde 75.001 a 85.000', 'rango desde 85.001 a 95.000',
               'mayores a 95.001']
     model_selected = st.sidebar.selectbox(
         '¿ Selecciona segmento ?', option)
 
     riesgo = porcentajeError(model_selected)
-    
-            
+
     st.subheader(f'segmento seleccionado: {model_selected} USD')
     st.subheader(f'tasa de error del modelo: {riesgo}')
     # st.subheader(df)
 
     if st.button('Valorizar'):
         result = None
-        
+
         # if model_selected == 'rango desde 0 a 25.000':
-        #     result = mdl_generalista.predict(df)        
+        #     result = mdl_generalista.predict(df)
         # if model_selected == 'rango desde 25.001 a 35.000':
-        #     result = mdl_premium1.predict(df)        
+        #     result = mdl_premium1.predict(df)
         if model_selected == 'rango desde 35.0001 a 45.000':
-            result = mdl_premium2.predict(df)             
+            result = mdl_premium2.predict(df)
         if model_selected == 'rango desde 45.001 a 55.000':
-            result = mdl_premium3.predict(df)             
+            result = mdl_premium3.predict(df)
         if model_selected == 'rango desde 55.001 a 65.000':
-            result = mdl_premium4.predict(df)        
+            result = mdl_premium4.predict(df)
         if model_selected == 'rango desde 65.001 a 75.000':
             result = mdl_premium5.predict(df)
         if model_selected == 'rango desde 75.001 a 85.000':
-            result = mdl_premium6.predict(df)        
+            result = mdl_premium6.predict(df)
         if model_selected == 'rango desde 85.001 a 95.000':
-            result = mdl_premium7.predict(df)            
+            result = mdl_premium7.predict(df)
         if model_selected == 'mayores a 95.001':
-            result = mdl_premium8.predict(df)        
-        
-        #st.success('predicción valor de compra: ${:.2f} usd'.format(result[0]))
+            result = mdl_premium8.predict(df)
+
+        # st.success('predicción valor de compra: ${:.2f} usd'.format(result[0]))
         result_format = format_currency(result[0], 'USD', locale='en_US')
-        sale_value = format_currency(result[0]*1.1, 'USD', locale='en_US')
+        sale_value = format_currency(
+            result[0]*((float(st.session_state.ganancia)/100)+1), 'USD', locale='en_US')
         st.success(f'predicción valor de compra: {result_format} USD')
         st.success(f'valor de venta recomendado: {sale_value} USD')
+
 
 if __name__ == '__main__':
     main()
